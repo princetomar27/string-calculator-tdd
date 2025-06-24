3ab519f3ba90631a40ce5cdbfa8dc7ac9ade7991
Prince Tomar <tomarprince443@gmail.com>
2025-06-24 11:01:16 +0530

    first test for empty string

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,1 +1,46 @@
-void main() {}
+class StringCalculator {
+  int add(String numbers) {
+    if (numbers.isEmpty) {
+      return 0;
+    }
+
+    String delimiter = ',';
+    String numbersToProcess = numbers;
+
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+      int newlineIndex = numbers.indexOf('\n');
+      if (newlineIndex != -1) {
+        delimiter = numbers.substring(2, newlineIndex);
+        numbersToProcess = numbers.substring(newlineIndex + 1);
+      }
+    }
+
+    // Replace newlines with the current delimiter for uniform processing
+    numbersToProcess = numbersToProcess.replaceAll('\n', delimiter);
+
+    // Split by delimiter and convert to numbers
+    List<String> numberStrings = numbersToProcess.split(delimiter);
+    List<int> nums = [];
+    List<int> negativeNumbers = [];
+
+    for (String numStr in numberStrings) {
+      if (numStr.isNotEmpty) {
+        int num = int.parse(numStr.trim());
+        if (num < 0) {
+          negativeNumbers.add(num);
+        }
+        nums.add(num);
+      }
+    }
+
+    // Check for negative numbers
+    if (negativeNumbers.isNotEmpty) {
+      String negativesList = negativeNumbers.join(', ');
+      throw Exception('negative numbers not allowed $negativesList');
+    }
+
+    // Sum all numbers
+    return nums.fold(0, (sum, num) => sum + num);
+  }
+}
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,1 +1,9 @@
+import 'package:string_calculator_kata/string_calculator.dart';
+import 'package:test/test.dart';
 
+void main() {
+  test('should return 0 for empty string', () {
+    var calculator = StringCalculator();
+    expect(calculator.add(''), equals(0));
+  });
+}
